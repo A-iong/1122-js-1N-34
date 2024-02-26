096fea90c3ebeda182644be2e4d0268aaec30174
A-iong <210410634@gms.tku.edu.tw>
2024-02-26 20:19:48 +0800

    W01-P4: implement multiply function

--- demo/w01_dom_34/js/w01_34.js
+++ demo/w01_dom_34/js/w01_34.js
@@ -1,9 +1,10 @@
 const userInput = document.querySelector('#input-number');
-
+const subtractBtn = document.querySelector('#btn-subtract');
 const addBtn = document.querySelector('#btn-add');
-const subtract = document.querySelector('#btn-btn-subtract');
-const multiply = document.querySelector('#btn-multiply');
-const divide = document.querySelector('#btn-divide');
+// const subtract = document.querySelector('#btn-btn-subtract');
+// const multiply = document.querySelector('#btn-multiply');
+const divideBtn = document.querySelector('#btn-divide');
+const multiplyBtn = document.querySelector('#btn-multiply');
 
 const currentCalculaitionOutput = document.querySelector('#current-calculation');
 const currentResultOutput = document.querySelector('#current-result');
@@ -35,3 +36,36 @@ function add () {
 }
 
 addBtn.addEventListener('click', add); 
+
+function subtract() {
+    const operand1 = currentResult;
+    const operand2 = getUserInput();
+    currentResult = operand1 - operand2;
+    console.log(`${operand1} - ${operand2} = ${currentResult}`);
+    const calText = `${operand1} - ${operand2}`;
+    OutputResult(currentResult, calText);
+}
+subtractBtn.addEventListener("click",subtract);
+
+function multiply() {
+    console.log(currentResult)
+    const operand1 = currentResult;
+    const operand2 = getUserInput();
+    currentResult = operand1 * operand2;
+    console.log(`${operand1} * ${operand2} = ${currentResult}`);
+    const calText = `${operand1} * ${operand2}`;
+    OutputResult(currentResult, calText);
+}
+multiplyBtn.addEventListener("click",multiply);
+
+
+function divide() {
+    console.log(currentResult)
+    const operand1 = currentResult;
+    const operand2 = getUserInput();
+    currentResult = operand1 / operand2;
+    console.log(`${operand1} / ${operand2} = ${currentResult}`);
+    const calText = `${operand1} / ${operand2}`;
+    OutputResult(currentResult, calText);
+}
+divideBtn.addEventListener("click",divide);
